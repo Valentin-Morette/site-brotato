79b166266e49802160a63a0c025ed5af9cd61428
valentin-morette <morette.valentin@gmail.com>
2025-05-10 19:16:48 +0200

    update deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
 	integrations: [tailwind()],
 	site: 'https://valentin-morette.github.io',
-	base: '/site-brotato',
+	base: 'site-brotato',
 });
